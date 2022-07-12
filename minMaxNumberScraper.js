e835b0e3b2e1f9af65bd44d04ac275eacb8eca02
Kim <kimly.jet@gmail.com>
2022-07-12 10:43:16 +0900

    Refactor: Convert splitted string elements to number before returning array

--- minMaxNumberScraper.js
+++ minMaxNumberScraper.js
@@ -1,13 +1,6 @@
 const minMaxNumScraper = (strOfNums) => {
-  // Create result array
-  const result = [];
-  // Convert strOfNums to array
-  const arrOfNums = strOfNums.split(" ");
-  // User Math.min and Math.max to get highest number of array
-  const lowestNum = Math.min(...arrOfNums);
-  const highestNum = Math.max(...arrOfNums);
-  // Push lowestNum and highestNum to result
-  result.push(lowestNum, highestNum);
-
-  return result;
+  // Convert strOfNums to array and each element to number
+  const arrOfNums = strOfNums.split(" ").map(Number);
+  // Return array with lowest and highest number
+  return [Math.min(...arrOfNums), Math.max(...arrOfNums)];
 };
