7077795c6a299fcb057c5eb7de82a95e8692f997
Kim <kimly.jet@gmail.com>
2022-07-14 09:50:35 +0900

    Docs: Add line break to description

--- countThe7s.js
+++ countThe7s.js
@@ -1,5 +1,6 @@
 // Takes in a number n. 
-// Return the number of 7's that occur if you count from 1 through n. A 7 can occur in any digit. For example, the number 2778 contains two 7's and these should be counted separately.
+// Return the number of 7's that occur if you count from 1 through n. 
+// A 7 can occur in any digit. For example, the number 2778 contains two 7's and these should be counted separately.
 
 const countSevens = (n) => {
   let count = 0;
