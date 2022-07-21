11dd7a33108d2cb0c9f10b95d2da432c1f861390
Kim <kimly.jet@gmail.com>
2022-07-21 10:53:06 +0900

    Refactor: Rename variables for clarity and refactor

--- saveKeysThatAreValues.js
+++ saveKeysThatAreValues.js
@@ -4,18 +4,14 @@ const saveKeysThatAreValues = (obj) => {
   // Create result object
   const result = {};
   // Store keys that are stored as values in obj
-  const keys = [];
-  // Loop over obj
-  for (const key in obj) {
-    // Store values in keys array
-    keys.push(obj[key]);
-  }
-  // Loop over keys array
-  for (const key of keys) {
-    // Check if key in keys array is a key in obj
-    if (key in obj) {
+  const values = Object.values(obj);
+  
+  // Loop over values array
+  for (const el of values) {
+    // Check if el in values array is a key in obj
+    if (el in obj) {
       // Assign key/value pair to result
-      result[key] = obj[key];
+      result[el] = obj[el];
     }
   }
   return result;
