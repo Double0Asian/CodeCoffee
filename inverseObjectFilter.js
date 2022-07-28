8194dc933694028d67e7ffaec3e101e709558c0b
Kim <kimly.jet@gmail.com>
2022-07-28 11:00:55 +0900

    Refactor: Refector for better time complexity

--- inverseObjectFilter.js
+++ inverseObjectFilter.js
@@ -1,14 +1,20 @@
 //takes in an object and an array of strings. The values in the object are strings. Create a copy of this object that only contains the keys/values if the value is NOT present in the array.
 
+// O(n*3) Linear time complexity
 const inverseObjectFilter = (obj, valueExclusionList) => {
-  const result = obj;
+  const swappedKeyValues = {};
+  const result = {};
+
+  for (const key in obj) {
+    swappedKeyValues[obj[key]] = key;
+  }
 
   for (const value of valueExclusionList) {
-    for (const key in obj) {
-      if (obj[key] === value) {
-        delete obj[key];
-      }
-    }
+    if (value in swappedKeyValues) delete swappedKeyValues[value];
+  }
+
+  for (const key in swappedKeyValues) {
+    result[swappedKeyValues[key]] = key;
   }
 
   return result;
